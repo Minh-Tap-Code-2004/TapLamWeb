28b8180cb5cd4d21bb17b4fe3fc0a07bf76deeb2
NhatMinh <146560329+Minh-Tap-Code-2004@users.noreply.github.com>
2023-10-06 22:07:40 +0700

    Add files via upload

---
+++ locations.php
@@ -0,0 +1,20 @@
+<?php 
+  include("header.html");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Locations</title>
+</head>
+<body>
+  Đây là trang địa chỉ<br>
+  LMAOLMAOLMAO<br>
+</body>
+</html>
+
+<?php 
+  include("footer.html");
+?>
