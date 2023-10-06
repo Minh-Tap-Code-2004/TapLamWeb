a5ca1d66594095ce5fd755ff9286941e00526c12
NhatMinh <146560329+Minh-Tap-Code-2004@users.noreply.github.com>
2023-10-06 22:18:16 +0700

    Add files via upload

---
+++ include.php
@@ -0,0 +1,23 @@
+<?php 
+//include() = Copies the content of a file (php/html/text) 
+//and includes it in your php file. Sections of our website
+//become reusable. Changes only need to be made in one place
+  include("header.html");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Web của Minh noob code =3</title>
+</head>
+<body>
+  Web này mới lập nên không có content :33<br>
+  Lmao Bủh Bủh Nỉgga HAHAHA<br>
+</body>
+</html>
+
+<?php 
+  include("footer.html");
+?>
